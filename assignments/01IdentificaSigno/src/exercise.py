bc1d4314c376fda189dad4e503ef7910d60b46ee
Aura Mejía <110926714+aura-mh@users.noreply.github.com>
2022-08-23 12:28:49 -0500

    Update exercise.py

--- assignments/01IdentificaSigno/src/exercise.py
+++ assignments/01IdentificaSigno/src/exercise.py
@@ -1,7 +1,15 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
-        
-
+    numero = int(input('Dame un numero: '))
+    def mensaje(numero):
+        if numero <0:
+            return 'Es negativo'
+        if numero == 0:
+            return 'Es cero'
+        if numero >0:
+            return 'Es positivo'
+    resultado = mensaje(numero)
+    print(resultado)
 if __name__ == '__main__':
     main()
